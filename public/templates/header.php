615acfb336f261b647e09f43c7f49043bdf79247
Fardeen Ehsan <fardeen.es7@gmail.com>
2022-01-04 11:00:45 +0600

    minor changes

--- public/templates/header.php
+++ public/templates/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,7 +12,7 @@
 
     <!-- Font awesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
+
     <!-- Custom CSS-->
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/single.css">
@@ -23,51 +24,58 @@
     <script src="/js/generate-vaccine-card.js"></script>
     <link rel="stylesheet" href="/icons/css/all.css">
     <style type="text/css" media="screen">
-
-        #vaccine-card-html{
+        #vaccine-card-html {
             visibility: hidden;
             padding: 0 30%;
             margin: 0;
-            }
-        #download-v-card-btn, #error{
-                visibility: visible;
-            }
-        header, .single-title-header, .body,footer{
-                background-color:#fff;
-            }
+        }
+
+        #download-v-card-btn,
+        #error {
+            visibility: visible;
+        }
+
+        header,
+        .single-title-header,
+        .body,
+        footer {
+            background-color: #fff;
+        }
     </style>
     <style type="text/css" media="print">
+        header,
+        .single-title-header,
+        #download-v-card-btn,
+        .body,
+        footer {
+            display: none;
+        }
 
-        header, .single-title-header,#download-v-card-btn, .body,footer{
-                display: none;
-            }
-        #vaccine-card-html{
-                background-color:#FFF;
-                margin: 3%;
-            }
+        #vaccine-card-html {
+            background-color: #FFF;
+            margin: 3%;
+        }
     </style>
 
 
     <title>VACSYS</title>
-    
+
 </head>
 
 <body>
 
     <!--Header -->
 
-    <header class="">  
+    <header class="">
         <nav class="navbar navbar-expand-md navbar-light">
-        <a  href="/" class="title navbar-brand">VACSYS</a>
-        <div class="navbar-link ms-auto">
-            <ul class="navbar-nav text-center">
-            <li class="nav-item"><a href="register.php" class="nav-link">Register</a></li>
-            <li class="nav-item"><a href="status.php" class="nav-link">Status</a></li>
-            <li class="nav-item"><a href="vaccine-card.php" class="nav-link">Vaccine Card</a></li>
-            <li class="nav-item"><a href="certificate.php" class="nav-link">Certificate</a></li>  
-            <li class="nav-item"><a href="admin-login.php" target="_blank" class="nav-link btn btn-warning">Admin Portal</a></li>  
-            </ul>
-               
-        </div>
-          </nav>
+            <a href="/" class="title navbar-brand">VACSYS</a>
+            <div class="navbar-link ms-auto">
+                <ul class="navbar-nav text-center">
+                    <li class="nav-item"><a href="register.php" class="nav-link">Register</a></li>
+                    <li class="nav-item"><a href="/user/login.php" class="nav-link">Login</a></li>
+                    <li class="nav-item"><a href="/admin/login.php" target="_blank" class="nav-link btn btn-warning">Admin Portal</a></li>
+                </ul>
+
+            </div>
+        </nav>
     </header>
--- public/user/login.php
+++ public/user/login.php
@@ -1,40 +1,39 @@
 <?php
-    session_start();
-    include_once "../php/config.php";
-    if(isset($_SESSION['nid'])){
-        header("Location: dashboard.php");
-    }
-    if(isset($_POST['nid'])){
-        
-        $nid = $_POST["nid"];
-        
-        $password = $_POST["password"];
-        $sql = "SELECT * FROM `user` WHERE nid = '$nid'";
-        $result = $conn->query($sql);
-    
-        if ($result->num_rows > 0) {
+session_start();
+include_once "../php/config.php";
+if (isset($_SESSION['nid'])) {
+    header("Location: dashboard.php");
+}
+if (isset($_POST['nid'])) {
+
+    $nid = $_POST["nid"];
+
+    $password = $_POST["password"];
+    $sql = "SELECT * FROM `user` WHERE nid = '$nid'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
         // output data of each row
-        while($row = $result->fetch_assoc()) {
-            if($password == $row["pass"]){
+        while ($row = $result->fetch_assoc()) {
+            if ($password == $row["pass"]) {
                 $_SESSION['nid'] = $_POST['nid'];
-                echo "<script>window.alert('NID: ".$_SESSION['nid'].".')</script>";
+                echo "<script>window.alert('NID: " . $_SESSION['nid'] . ".')</script>";
                 header("Location: dashboard.php");
-            }
-            else{
+            } else {
                 echo "<script>window.alert('NID or Password error.')</script>";
             }
         }
-        }else
-        {
-            echo "<script>window.alert('NID or Password error.')</script>";
-        }
-        mysqli_close($conn);
+    } else {
+        echo "<script>window.alert('NID or Password error.')</script>";
     }
-    
+    mysqli_close($conn);
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -45,45 +44,47 @@
 
     <!-- Font awesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
+
     <!-- Custom CSS-->
     <link rel="stylesheet" href="../css/user-login.css">
     <title>User Login | VACSYS</title>
 </head>
+
 <body>
-    
 
-<div class="row vh-100 mw-100 mh-100">
-    <div class="h-100 m-0 left-side col-xs-1 text-white"  align="center">
-        <h1 class="fw-bolder">VACSYS</h1>
-        <br>
-        <h5>Free Vaccination Program.</h5>
-        <p class="not-admin">Not Registered? <a href="/register.php">Click here</a></p>
 
-    </div>
+    <div class="row vh-100 mw-100 mh-100">
+        <div class="h-100 m-0 left-side col-xs-1 text-white" align="center">
+            <h1 class="fw-bolder">VACSYS</h1>
+            <br>
+            <h5>Free Vaccination Program.</h5>
+            <p class="not-admin">Not Registered? <a href="/register.php">Click here</a></p>
 
-    <div class="h-100 m-25 right-side col-xs-1 text-dark" align="center">
-        <div class="container">
-            <form action="" method="POST">
-                <h3>Welcome back.</h3>
-                <h5>Admin Login</h5>
-                <div class="form-floating mb-3">
-                <input type="number" class="form-control" name="nid" id="nid" placeholder="0000000000">
-                <label for="floatingInput">NID number</label>
-                </div>
-                <div class="form-floating">
-                <input type="password" class="form-control" name="password" id="password" placeholder="Password">
-                <label for="floatingPassword">Password</label>
-                </div>
-                <div class="form-floating">
-                    <button class="btn btn-primary" type="submit">Log in</button>
-                </div>
-                <p>Trouble logging in? <a href="#">Click here.</a></p>
-            </form>
         </div>
-    </div>
 
-</div>
+        <div class="h-100 m-25 right-side col-xs-1 text-dark" align="center">
+            <div class="container">
+                <form action="" method="POST">
+                    <h3>Welcome back.</h3>
+                    <h5>User Login</h5>
+                    <div class="form-floating mb-3">
+                        <input type="number" class="form-control" name="nid" id="nid" placeholder="0000000000">
+                        <label for="floatingInput">NID number</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                    </div>
+                    <div class="form-floating">
+                        <button class="btn btn-primary" type="submit">Log in</button>
+                    </div>
+                    <p>Trouble logging in? <a href="#">Click here.</a></p>
+                </form>
+            </div>
+        </div>
+
+    </div>
 
 </body>
+
 </html>
